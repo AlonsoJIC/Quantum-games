c8b8d7db97de8f234df3156799ef127e6d5b562c
AlonsoJIC <jalonsojic@gmail.com>
2024-01-09 17:35:15 -0600

    v0.2.3 - Nuevo boton reactivo, eliminados archivos varios, y agregada page para register junto a form.

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -10,6 +10,7 @@ import { AuthService } from '@services/auth.service';
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.scss']
 })
 export class RegisterFormComponent {
 
--- src/app/services/me.service.ts
+++ src/app/services/me.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
 import { User } from '@models/user.model';
-import { Board } from '@models/board.model';
 import { checkToken } from '@interceptors/token.interceptor';
 
 @Injectable({
@@ -16,11 +15,6 @@ export class MeService {
 
   getMeProfile() {
     return this.http.get<User>(`${this.apiUrl}/api/v1/me/profile`, {
-      context: checkToken(),
-    });
-  }
-  getMeBoards() {
-    return this.http.get<Board[]>(`${this.apiUrl}/api/v1/me/boards`, {
       context: checkToken(),
     });
   }
